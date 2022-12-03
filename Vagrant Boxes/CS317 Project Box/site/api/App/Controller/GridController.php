15bfad71f45487df932f86fb07f4224fb8c22608
George Waldron <waldrog2@my.erau.edu>
2022-12-02 18:59:34 -0700

    Fix Deleted React VM

--- Vagrant Boxes/CS317 Project Box/site/api/App/Controller/GridController.php
+++ Vagrant Boxes/CS317 Project Box/site/api/App/Controller/GridController.php
@@ -16,24 +16,14 @@
             $req = new Request();
             $parameters = $req->parseQueryString($params);
 //            var_dump(array_key_exists('row_number',$parameters));
-            if ( (array_key_exists('row_number',$parameters)) && (array_key_exists('row_width',$parameters)))
-            {
-//                echo "Here!";
-                $desired_row = $parameters['row_number'];
-                $row_width = $parameters['row_width'];
-                $max_rows = ceil(Album::getAlbumCount() / $row_width);
-                if ($desired_row > $max_rows || $desired_row < 0)
-                {
-                    $resp = new Response();
-                    $resp->send(400,"Bad Request");
-                }
 
-                $starting_album_id = 1 + ($row_width * $desired_row);
-                $ending_album_id = $starting_album_id + $row_width;
+//                echo "Here!";
+                $max_album_id = Album::getAlbumCount();
+
+                $starting_album_id = 1;
+                $ending_album_id = $max_album_id - 1;
 
                 $grid_row_data = [
-                    'max_rows' => $max_rows,
-                    'entries' => []
                 ];
                 for ($album_id = $starting_album_id; $album_id < $ending_album_id; $album_id++)
                 {
@@ -57,16 +47,5 @@
                 }
                 $resp = new Response();
                 $resp->sendJSON($grid_row_data);
-            }
-
-            else
-            {
-                $resp = new Response();
-                $resp->send(400,"Bad Request");
-            }
-
-
-
-
         }
     }
