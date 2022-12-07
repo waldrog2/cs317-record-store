01997d70b5acaeb9f1dfb2a994bfbcb1a9cfaae6
BrandonW-ERAU <willisob@my.erau.educ>
2022-12-06 17:57:09 -0700

    Tracklist

--- Home Page/my-app/src/album-page.js
+++ Home Page/my-app/src/album-page.js
@@ -2,6 +2,20 @@ import React from "react";
 import "./css/album-page.css";
 import "./css/home-page-header.css";
 export default function AlbumPage(props) {
+  const { id } = useParams();
+  const [albumData, updateAlbumData] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:8044/api/album?id=" + id).then((res) =>
+      res.json().then((data) => {
+        updateAlbumData(data);
+      })
+    );
+  }, []);
+
+  const songs = albumList.songs.map((item) => {
+    return <TrackList key={item.name} item={item} />;
+  });
   return (
     <div>
       <nav className="header">
@@ -24,19 +38,24 @@ export default function AlbumPage(props) {
           </button>
         </div>
       </nav>
-
-      <div className="album-page">
-        <div className="album-page-image">
-          <img
-            className="album-page-cover"
-            src={props.item.art_link}
-            alt="Album Art"
-          />
+      <div className="container">
+        <div className="album-page">
+          <div className="album-page-image">
+            <img
+              className="album-page-cover"
+              src={props.item.art_link}
+              alt="Album Art"
+            />
+          </div>
+          <div className="album-page-info">
+            <p className="album-page-title">{props.item.title}</p>
+            <p className="album-page-artist">{props.item.artist}</p>
+            <p className="album--pagedate">{props.item.release_date}</p>
+          </div>
         </div>
-        <div className="album-page-info">
-          <p className="album-page-title">{props.item.title}</p>
-          <p className="album-page-artist">{props.item.artist}</p>
-          <p className="album--pagedate">{props.item.release_date}</p>
+
+        <div className="song-list">
+          <section className="song-list">{songs}</section>
         </div>
       </div>
     </div>
---
+++ Home Page/my-app/src/tracklist.js
@@ -0,0 +1,4 @@
+export default function TrackList(props) {
+  console.log(props);
+  return <p>props.item.songs.name</p>;
+}
